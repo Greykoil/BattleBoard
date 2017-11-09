396a3179784b92d52686d8978fcb1abe511d566c
Alex Spencer <alex.i.j.spencer@gmail.com>
2017-11-09 22:04:07 +0000

    Put back missing file
    Add file that somehow got missed in the last commit

--- BattleBoard/SkillWindow.cpp
+++ BattleBoard/SkillWindow.cpp
@@ -1,26 +1,38 @@
 //=============================================================================
-//D The apps main window
-//
-// Contains a tab list of the windows where things actually happen
+//D The top level window that displays the list of availble skills to the user
 //
 //-----------------------------------------------------------------------------
 
 // Class header include
 #include "SkillWindow.h"
-
+#include "SkillPageVMManager.h"
 // includes from project
-
+#include "SkillWidget.h"
 //includes from QT
 
 //=============================================================================
-SkillWindow::SkillWindow(QWidget *parent)
+SkillWindow::SkillWindow(
+  SkillPageVMManager* manager,
+  QWidget* parent
+)
 //
 //D Default constructor
 //
 //-----------------------------------------------------------------------------
-: QDialog(parent)
+  : QDialog(parent),
+    m_vm_manager(manager)
 {
   m_ui.setupUi(this);
+
+
+  // Get a list of the visable skills
+  m_vm_manager->initial_skill_list(m_visable_skills);
+
+  // Here is a random list of skills to choose from
+  for (auto& skill : m_visable_skills) {
+    m_ui.verticalLayout_2->addWidget(skill.get());
+  }
+
 }
 
 //=============================================================================
